c250a9808e970872c906e942a63d4cc5492bdbe0
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2019-10-17 23:23:27 +0200

    CI test

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -4,18 +4,16 @@ file=nuget_packages.txt
 rm -f $file
 git diff --dirstat=files,0 HEAD~1 | sed 's/^[ 0-9.]\+% //g' >> $file
 
-cat $file
-
-# while read f; do
-#   if [[ $f == src/Convey.*/src/Convey.* ]];
-#   then
-#     dir="$f../../scripts"
-#     if [ -d $dir ]; then
-#       echo Publishing NuGet package: $f
-#       exec ./$dir/dotnet-pack.sh &
-#       wait
-#     fi
-#   fi
-# done <$file | sort | uniq
+while read f; do
+  if [[ $f == src/Convey.*/src/Convey.* ]];
+  then
+    dir="$f../../scripts"
+    if [ -d $dir ]; then
+      echo Publishing NuGet package: $f
+      exec ./$dir/dotnet-pack.sh &
+      wait
+    fi
+  fi
+done <$file | sort | uniq
 
 echo Finished publishing NuGet packages.

420a0e9cf1f53df4dafc83bda827e59d13f87e23
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2019-10-17 23:07:36 +0200

    Auto package script update

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
+echo Preparing NuGet packages...
 file=nuget_packages.txt
 rm -f $file
-git diff --dirstat=files,0 HEAD~3 | sed 's/^[ 0-9.]\+% //g' >> $file
+git diff --dirstat=files,0 HEAD~1 | sed 's/^[ 0-9.]\+% //g' >> $file
 
 while read f; do
   if [[ $f == src/Convey.*/src/Convey.* ]];
   then
     dir="$f../../scripts"
     if [ -d $dir ]; then
-      echo "Publishing a Nuget package: $f"
+      echo Publishing NuGet package: $f
       exec ./$dir/dotnet-pack.sh &
       wait
     fi
   fi
 done <$file | sort | uniq
+
+echo Finished publishing NuGet packages.

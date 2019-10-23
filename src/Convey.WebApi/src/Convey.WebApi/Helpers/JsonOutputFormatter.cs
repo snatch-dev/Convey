a90d5e5dee87ef7d140f923fb2fc3dd16fd5614a
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2019-10-23 22:25:06 +0200

    Fixed null json output

--- src/Convey.WebApi/src/Convey.WebApi/Helpers/JsonOutputFormatter.cs
+++ src/Convey.WebApi/src/Convey.WebApi/Helpers/JsonOutputFormatter.cs
@@ -7,7 +7,7 @@ namespace Convey.WebApi.Helpers
     internal class JsonOutputFormatter : IOutputFormatter
     {
         private readonly IJsonFormatterResolver _resolver;
-        
+
         public JsonOutputFormatter(IJsonFormatterResolver resolver)
         {
             _resolver = resolver;
@@ -21,7 +21,11 @@ namespace Convey.WebApi.Helpers
         public Task WriteAsync(OutputFormatterWriteContext context)
         {
             context.HttpContext.Response.ContentType = "application/json";
-
+            if (context.Object is null)
+            {
+                return Task.CompletedTask;
+            }
+            
             return context.ObjectType == typeof(object)
                 ? JsonSerializer.NonGeneric.SerializeAsync(context.HttpContext.Response.Body, context.Object, _resolver)
                 : JsonSerializer.NonGeneric.SerializeAsync(context.ObjectType, context.HttpContext.Response.Body,

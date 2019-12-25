fd37e21cc8942fcf20e9feb2191909a36c60b14b
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2019-12-25 13:44:01 +0100

    Mongo outbox fixed mapping

--- src/Convey.MessageBrokers.Outbox.Mongo/src/Convey.MessageBrokers.Outbox.Mongo/Extensions.cs
+++ src/Convey.MessageBrokers.Outbox.Mongo/src/Convey.MessageBrokers.Outbox.Mongo/Extensions.cs
@@ -2,6 +2,7 @@ using Convey.MessageBrokers.Outbox.Messages;
 using Convey.MessageBrokers.Outbox.Mongo.Internals;
 using Convey.Persistence.MongoDB;
 using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Bson.Serialization;
 
 namespace Convey.MessageBrokers.Outbox.Mongo
 {
@@ -12,20 +13,27 @@ namespace Convey.MessageBrokers.Outbox.Mongo
         {
             var builder = configurator.Builder;
             var options = configurator.Options;
-            
+
             var inboxCollection = string.IsNullOrWhiteSpace(options.InboxCollection)
                 ? "inbox"
                 : options.InboxCollection;
             var outboxCollection = string.IsNullOrWhiteSpace(options.OutboxCollection)
                 ? "outbox"
                 : options.OutboxCollection;
-            
+
             builder.AddMongo(mongoSectionName);
             builder.AddMongoRepository<InboxMessage, string>(inboxCollection);
             builder.AddMongoRepository<OutboxMessage, string>(outboxCollection);
             builder.Services.AddTransient<IMessageOutbox, MongoMessageOutbox>();
             builder.Services.AddTransient<MongoOutboxInitializer>();
             builder.AddInitializer<MongoOutboxInitializer>();
+
+            BsonClassMap.RegisterClassMap<OutboxMessage>(m =>
+            {
+                m.AutoMap();
+                m.UnmapMember(p => p.Message);
+                m.UnmapMember(p => p.MessageContext);
+            });
 
             return configurator;
         }

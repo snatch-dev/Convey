84c62648614cfdc937d1635af2dbc847c6139a95
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2020-02-02 23:45:35 +0100

    Vault lease renewals refactoring

--- src/Convey.Secrets.Vault/src/Convey.Secrets.Vault/Internals/VaultHostedService.cs
+++ src/Convey.Secrets.Vault/src/Convey.Secrets.Vault/Internals/VaultHostedService.cs
@@ -8,13 +8,12 @@ using VaultSharp;
 
 namespace Convey.Secrets.Vault.Internals
 {
-    internal sealed class VaultHostedService : IHostedService
+    internal sealed class VaultHostedService : BackgroundService
     {
         private readonly IVaultClient _client;
         private readonly ILeaseService _leaseService;
         private readonly VaultOptions _options;
         private readonly ILogger<VaultHostedService> _logger;
-        private Timer _timer;
         private readonly int _interval;
 
         public VaultHostedService(IVaultClient client, ILeaseService leaseService, VaultOptions options,
@@ -27,61 +26,45 @@ namespace Convey.Secrets.Vault.Internals
             _interval = 5;
         }
 
-        public Task StartAsync(CancellationToken cancellationToken)
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             if (!_options.Enabled || _options.Lease is null || !_options.Lease.Any(l => l.Value.AutoRenewal))
             {
-                return Task.CompletedTask;
+                return;
             }
 
-            _logger.LogInformation($"Vault is enabled, processing lease renewals every {_interval} s.");
-            _timer = new Timer(ProcessLease, null, TimeSpan.Zero, TimeSpan.FromSeconds(_interval));
-            return Task.CompletedTask;
-        }
-
-        private void ProcessLease(object state)
-        {
-            _ = ProcessLeaseAsync();
-        }
-
-        private async Task ProcessLeaseAsync()
-        {
-            var now = DateTime.UtcNow;
-            var nextIterationAt = now.AddSeconds(2 * _interval);
-            foreach (var (key, lease) in _leaseService.All.Where(l => l.Value.AutoRenewal))
+            _logger.LogInformation($"Vault lease renewals will be validated every {_interval} s.");
+            var interval = TimeSpan.FromSeconds(_interval);
+            while (!stoppingToken.IsCancellationRequested)
             {
-                if (lease.ExpiryAt > nextIterationAt)
+                var now = DateTime.UtcNow;
+                var nextIterationAt = now.AddSeconds(2 * _interval);
+                foreach (var (key, lease) in _leaseService.All.Where(l => l.Value.AutoRenewal))
                 {
-                    continue;
+                    if (lease.ExpiryAt > nextIterationAt)
+                    {
+                        continue;
+                    }
+
+                    _logger.LogInformation($"Renewing a lease with ID: '{lease.Id}', for: '{key}', " +
+                                           $"duration: {lease.Duration} s.");
+                    var renewedLease = await _client.V1.System.RenewLeaseAsync(lease.Id, lease.Duration);
+                    lease.Refresh(renewedLease.LeaseDurationSeconds);
                 }
 
-                _logger.LogInformation($"Renewing a lease with ID: '{lease.Id}', for: '{key}', " +
-                                       $"duration: {lease.Duration} s.");
-                var renewedLease = await _client.V1.System.RenewLeaseAsync(lease.Id, lease.Duration);
-                lease.Refresh(renewedLease.LeaseDurationSeconds);
-            }
-        }
-
-        public Task StopAsync(CancellationToken cancellationToken)
-        {
-            if (!_options.Enabled)
-            {
-                return Task.CompletedTask;
+                await Task.Delay(interval, stoppingToken);
             }
 
-            _timer?.Change(Timeout.Infinite, 0);
             if (!_options.RevokeLeaseOnShutdown)
             {
-                return Task.CompletedTask;
+                return;
             }
 
             foreach (var (key, lease) in _leaseService.All)
             {
                 _logger.LogInformation($"Revoking a lease with ID: '{lease.Id}', for: '{key}'.");
-                _client.V1.System.RevokeLeaseAsync(lease.Id);
+                await _client.V1.System.RevokeLeaseAsync(lease.Id);
             }
-
-            return Task.CompletedTask;
         }
     }
 }

e70307b3aad1b3ececad58547304a6a1938bcbc2
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2020-04-12 17:23:24 +0200

    Fixed timestamp

--- src/Convey.Auth/src/Convey.Auth/Dates/Extensions.cs
+++ src/Convey.Auth/src/Convey.Auth/Dates/Extensions.cs
@@ -4,12 +4,6 @@ namespace Convey.Auth.Dates
 {
     internal static class Extensions
     {
-        public static long ToTimestamp(this DateTime dateTime)
-        {
-            var centuryBegin = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-            var expectedDate = dateTime.Subtract(new TimeSpan(centuryBegin.Ticks));
-
-            return expectedDate.Ticks / 10000;
-        }
+        public static long ToTimestamp(this DateTime dateTime) => new DateTimeOffset(dateTime).ToUnixTimeSeconds();
     }
 }

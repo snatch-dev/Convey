2f6c31bd0183728428566e1ef73a7323cb73bc45
Mehrdad <m92.moazami@gmail.com>
2020-09-17 20:17:32 +0430

    handle sorting in pagination

--- src/Convey.Persistence.MongoDB/src/Convey.Persistence.MongoDB/Pagination.cs
+++ src/Convey.Persistence.MongoDB/src/Convey.Persistence.MongoDB/Pagination.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Convey.CQRS.Queries;
 using MongoDB.Driver;
@@ -9,10 +10,10 @@ namespace Convey.Persistence.MongoDB
     public static class Pagination
     {
         public static async Task<PagedResult<T>> PaginateAsync<T>(this IMongoQueryable<T> collection, IPagedQuery query)
-            => await collection.PaginateAsync(query.Page, query.Results);
+            => await collection.PaginateAsync(query.OrderBy, query.SortOrder, query.Page, query.Results);
 
-        public static async Task<PagedResult<T>> PaginateAsync<T>(this IMongoQueryable<T> collection,
-            int page = 1, int resultsPerPage = 10)
+        public static async Task<PagedResult<T>> PaginateAsync<T>(this IMongoQueryable<T> collection, string orderBy,
+            string sortOrder, int page = 1, int resultsPerPage = 10)
         {
             if (page <= 0)
             {
@@ -29,7 +30,12 @@ namespace Convey.Persistence.MongoDB
             }
             var totalResults = await collection.CountAsync();
             var totalPages = (int)Math.Ceiling((decimal)totalResults / resultsPerPage);
-            var data = await collection.Limit(page, resultsPerPage).ToListAsync();
+
+            List<T> data;
+            if (sortOrder.ToLower() == "asc")
+                data = await collection.OrderBy(x => orderBy).Limit(page, resultsPerPage).ToListAsync();
+            else
+                data = await collection.OrderByDescending(x => orderBy).Limit(page, resultsPerPage).ToListAsync();
 
             return PagedResult<T>.Create(data, page, resultsPerPage, totalPages, totalResults);
         }

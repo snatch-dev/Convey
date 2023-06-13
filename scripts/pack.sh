8968466c318e9eb8de96d02f8a94574296fb30dd
spetz <piotr.gankiewicz@gmail.com>
2023-06-13 10:57:25 +0200

    NuGet packages action fix

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -3,6 +3,7 @@ for dir in src/*/
 do
     dir=${dir%*/}
     echo Publishing NuGet package:  ${dir##*/}
+    chmod +x ./$dir/scripts/dotnet-pack.sh
     exec ./$dir/scripts/dotnet-pack.sh &
     wait
 done
